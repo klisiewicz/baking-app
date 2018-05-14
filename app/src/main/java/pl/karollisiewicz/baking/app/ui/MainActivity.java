022751f12fec5cceac5608036cc787840606b58e
Karol Lisiewicz <karol.lisiewicz@gmail.com>
2018-05-14 21:33:06 +0200

    Main activity along with main fragment created.

--- app/src/main/java/pl/karollisiewicz/baking/app/ui/MainActivity.java
+++ app/src/main/java/pl/karollisiewicz/baking/app/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package pl.karollisiewicz.baking;
+package pl.karollisiewicz.baking.app.ui;
 
 import android.support.v7.app.AppCompatActivity;
 
 import org.androidannotations.annotations.EActivity;
+
+import pl.karollisiewicz.baking.R;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
